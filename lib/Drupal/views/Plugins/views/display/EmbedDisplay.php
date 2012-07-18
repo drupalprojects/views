75cdfd6954cde3de9506af293977e9aed42d3160
Damian Lee <damian@damoweb.co.uk>
2012-07-18 14:13:11 +0100

    Added back export_ui plugins folder, Added EmbedDisplay plugin

---
+++ lib/Drupal/views/Plugins/views/display/EmbedDisplay.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @file
+ * Definition of Drupal\views\Plugins\views\display\FeedDisplay.
+ */
+
+namespace Drupal\views\Plugins\views\display;
+
+/**
+ * The plugin that handles an embed display.
+ *
+ * @ingroup views_display_plugins
+ */
+class EmbedDisplay extends DisplayPlugin {
+  // This display plugin does nothing apart from exist.
+}
