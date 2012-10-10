788b5bca51560ac2cfbe1025d7e669efe45151f2
xjm <xjm@65776.no-reply.drupal.org>
2012-10-10 10:52:33 -0500

    Remove mainpage, reference to Advanced Help, and unused @defgroup from views.api.php.

--- views.api.php
+++ views.api.php
@@ -6,31 +6,6 @@
  */
 
 /**
- * @mainpage Views 3 API Manual
- *
- * Much of this information is actually stored in the advanced help; please
- * check the API topic. This help will primarily be aimed at documenting
- * classes and function calls.
- *
- * Topics:
- * - @link views_lifetime The life of a view @endlink
- * - @link views_hooks Views hooks @endlink
- * - @link views_handlers About Views handlers @endlink
- * - @link views_plugins About Views plugins @endlink
- * - @link views_templates Views template files @endlink
- * - @link views_module_handlers Views module handlers @endlink
- */
-
-/**
- * @defgroup views_lifetime The life of a view
- * @{
- * This page explains the basic cycle of a view and what processes happen.
- *
- * @todo.
- * @}
- */
-
-/**
  * @defgroup views_handlers About Views handlers
  * @{
  * In Views, a handler is an object that is part of the view and is part of the
